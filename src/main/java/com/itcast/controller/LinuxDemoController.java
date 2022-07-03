1df75e7feba7db895cd962642ef0c1454fb20a91
itcast <hello@itcast.cn>
2022-07-03 18:53:17 +0200

    linux_demo

---
+++ src/main/java/com/itcast/controller/LinuxDemoController.java
@@ -0,0 +1,16 @@
+package com.itcast.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/linux")
+public class LinuxDemoController {
+
+    @GetMapping("")
+    public String hello(){
+        System.out.println("hello world...");
+        return "OK git";
+    }
+}
---
+++ src/test/java/com/itcast/linuxdemo/LinuxdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itcast.linuxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
