0b9468f2f9995d6da2a8a36cf46902593900d61a
itcast <hello@itcast.cn>
2022-07-03 21:19:20 +0200

    linux_demo 2

--- src/main/java/com/itcast/controller/LinuxDemoController.java
+++ src/main/java/com/itcast/controller/LinuxDemoController.java
@@ -11,6 +11,6 @@ public class LinuxDemoController {
     @GetMapping("")
     public String hello(){
         System.out.println("hello world...");
-        return "OK linuxdemo git";
+        return "OK linuxdemo git ps -ef|grep tomcat";
     }
 }
